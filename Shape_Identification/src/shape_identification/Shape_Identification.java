05eb1da0c654655d07cd29c90d379c379d08864b
AxelADN <tu.tamal.cocido@gmail.com>
2023-04-12 17:35:56 -0600

    Created V1 class

--- Shape_Identification/src/shape_identification/Shape_Identification.java
+++ Shape_Identification/src/shape_identification/Shape_Identification.java
@@ -14,7 +14,8 @@ public class Shape_Identification {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        V1 v1 = new V1();
+        
     }
     
 }
